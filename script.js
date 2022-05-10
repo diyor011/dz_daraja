6a15a95db7cc79b5c7a4401647d3fc2ccc4d2b5a
dior011 <diyorraxmatullaev3@gmail.com>
2022-05-10 15:34:19 +0500

    js_daraja

---
+++ script.js
@@ -0,0 +1,14 @@
+let num = +prompt ('son kiriting')
+let num1 = +prompt ('daraga kiriting')
+
+
+
+extra = 1 
+
+for(let i = 0; i<num1; ++i) {
+ extra = extra * num    
+ console.info (extra)
+}
+alert(extra)
+
+
